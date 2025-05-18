b2d93d98e6a17a61b649dcaaadceefeb2a5713c2
Your Name <your.email@example.com>
2025-05-18 22:21:02 +0300

    1.1.0 backend-logout route-controller

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { loginStudent } from "../controllers/auth.controller.js";
+import { loginStudent, logoutStudent } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.route("/login").post(loginStudent);
+router.route("/logout").post(logoutStudent);
 
 export default router;
